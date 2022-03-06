999461c0bf9d610663f77c36bbc65e223c9591cd
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-03-06 10:55:19 -0500

    update on 1m training

---
+++ bash/run_gpt2_on_miniBERTa_1M_v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=mini
+#SBATCH --time=2-12:00:00
+#SBATCH --gres=gpu:RTXA6000:2
+#SBATCH --ntasks=1
+#SBATCH --mem=120G
+#SBATCH --mail-type=ALL
+#SBATCH --exclude node017,node018
+#SBATCH --mail-user=ehoseini@mit.edu
+#SBATCH --partition=evlab
+
+. ~/.bash_profile
+conda activate gpt_neox
+echo $(which python)
+cd /om/user/ehoseini/gpt-neox/
+
+python ./deepy.py train.py -d configs gpt2.yml miniBERTA_1m_v2_setup.yml miniBERTa_1m_v2_gpt2_logging_setup.yml
