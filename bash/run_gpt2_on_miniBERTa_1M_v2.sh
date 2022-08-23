f3260963ecfa2e2ddc97b0dc5148cfea46526aab
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-22 20:10:21 -0400

    bugfix

--- bash/run_gpt2_on_miniBERTa_1M_v2.sh
+++ bash/run_gpt2_on_miniBERTa_1M_v2.sh
@@ -2,13 +2,16 @@
 #SBATCH --job-name=mini
 #SBATCH --time=2-12:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=120G
+#SBATCH --mem=64G
 #SBATCH --nodelist=node105
 #SBATCH --gres=gpu:A100:4
 #SBATCH --mail-type=ALL
 #SBATCH --exclude node017,node018
 #SBATCH --mail-user=ehoseini@mit.edu
 #SBATCH --partition=evlab
+
+XDG_CACHE_HOME=/om2/user/`whoami`/st
+export XDG_CACHE_HOME
 
 . ~/.bash_profile
 conda activate gpt_neox
