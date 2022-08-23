4128424f396d6b23f19751eeb646b53cac247dda
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-08-23 15:34:41 -0400

    bugfix

--- bash/run_gpt2_on_miniBERTa_1M_v2.sh
+++ bash/run_gpt2_on_miniBERTa_1M_v2.sh
@@ -3,12 +3,12 @@
 #SBATCH --time=2-12:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=120G
-#SBATCH --nodelist=node105
-#SBATCH --gres=gpu:A100:2
+
+#SBATCH --gres=gpu:RTXA6000:1
 #SBATCH --mail-type=ALL
 #SBATCH --exclude node017,node018
 #SBATCH --mail-user=ehoseini@mit.edu
-#SBATCH --partition=evlab
+
 
 XDG_CACHE_HOME=/om2/user/`whoami`/st
 export XDG_CACHE_HOME
@@ -19,3 +19,6 @@ echo $(which python)
 cd /om/user/ehoseini/gpt-neox/
 
 python ./deepy.py train.py -d configs gpt2.yml miniBERTA_1m_v2_setup.yml miniBERTa_1m_v2_gpt2_logging_setup.yml
+
+###SBATCH --partition=evlab
+###SBATCH --nodelist=node105
