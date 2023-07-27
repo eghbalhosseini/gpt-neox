5dadb19e0806861cfd6bbfbae8f245659e4d5052
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2023-07-27 11:04:29 -0400

    bugfix

---
+++ bash/bash_script.sh
@@ -0,0 +1,22 @@
+export PIP_DOWNLOAD_CACHE=/om2/user/ehoseini/.pip/cache
+export XDG_CACHE_HOME=/om2/user/ehoseini/st
+export SINGULARITY_CACHEDIR=/om2/user/ehoseini/st
+export OMPI_MCA_opal_cuda_support=true
+export CUDA_HOME=/cm/shared/openmind/cuda/11.3/
+module load openmind/cuda/11.3
+module load openmind/gcc/5.3.0
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/om/user/ehoseini/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+eval "$__conda_setup"
+else
+if [ -f "/om/user/ehoseini/miniconda3/etc/profile.d/conda.sh" ]; then
+. "/om/user/ehoseini/miniconda3/etc/profile.d/conda.sh"
+else
+export PATH="/om/user/ehoseini/miniconda3./bin:$PATH"
+fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
---
+++ tools/preprocess_miniberta_50M.py
@@ -0,0 +1,73 @@
+from datasets import load_dataset
+import os
+from tqdm import tqdm
+import jsonlines
+miniBERTa_dir='/om2/user/ehoseini/MyData/miniBERTa_v2/'
+#miniBERTa_dir='/Users/eghbalhosseini/Desktop/miniBERTa/'
+miniBERTa_vers=['miniBERTa-50M',]
+import subprocess
+from pathlib import Path
+import os
+from datasets import load_dataset
+
+#/om/user/ehoseini/MyData/miniBERTa/miniBERTa.py
+import sys
+if __name__ == '__main__':
+    # first step is changing format to jsonl
+    file_list_to_compress=[]
+    # print HF_DATASETS_CACHE locaiton
+
+    # check if HF_DATASETS_CACHE is set
+
+
+    for idx, vers in tqdm(enumerate(miniBERTa_vers)):
+        data=load_dataset(miniBERTa_dir,vers,cache_dir='/om2/user/ehoseini/.cache/huggingface/datasets')
+        for key in data.keys():
+            if not os.path.exists(os.path.join(miniBERTa_dir,vers,vers+'_'+key+'.jsonl.zst')):
+                if not os.path.exists(os.path.join(miniBERTa_dir,vers,vers+'_'+key+'.jsonl')):
+                    with jsonlines.open(os.path.join(miniBERTa_dir,vers,vers+'_'+key+'.jsonl'),'w') as writer:
+                        for idy, x in tqdm(enumerate(data[key])):
+                            if len(x['text'])>0:
+                                writer.write(x)
+                        writer.close()
+                # compress file
+                key='validation'
+                unix_str = f"zstd -f {os.path.join(miniBERTa_dir, vers, vers + '_' + key + '.jsonl')}"
+                cmd = f'''
+                #. ~/.bash_profile
+                conda activate gpt_neox
+                {unix_str}
+                '''
+                subprocess.call(cmd, shell=True, executable='/bin/bash')
+
+    file_list_to_preproc = []
+    for idx, vers in tqdm(enumerate(miniBERTa_vers)):
+        #data=load_dataset(miniBERTa_dir,vers)
+        keys=['train','validation','test']
+        for key in keys:
+            file_list_to_preproc.append(os.path.join(miniBERTa_dir,vers,vers+'_'+key+'.jsonl.zst'))
+
+    for idx, file in tqdm(enumerate(file_list_to_preproc)):
+        p = Path(file)
+        filename=p.name.replace('.jsonl.zst','')
+
+        unix_str = f"python tools/preprocess_data.py \
+            --input {file} \
+            --output-prefix {str(p.parent)}/{filename} \
+            --vocab data/gpt2-vocab.json \
+            --dataset-impl mmap \
+            --tokenizer-type GPT2BPETokenizer \
+            --merge-file data/gpt2-merges.txt \
+            --append-eod \
+            --workers 4 \
+            --ftfy "
+
+        cmd = f'''
+                        #. ~/.bash_profile
+                        source /om/user/ehoseini/miniconda3/etc/profile.d/conda.sh
+                        conda init
+                        conda activate gpt_neox
+                        {unix_str}
+                        '''
+        subprocess.call(cmd, shell=True, executable='/bin/bash')
+
